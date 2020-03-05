04d70619a90839ff04d817cc6a1a93e5b1718776
MROD <mriguez777@gmail.com>
2020-03-04 17:50:58 -0700

    I have coded the variables, questions and loop to run the game. I also linked the js script to the html page.

---
+++ assets/javascript/app.js
@@ -0,0 +1,44 @@
+
+//starting out with my global variables. I learned that I can place a prompt within the variable and run this in an  array. So this variable contains all my questions and answers.
+var question = [
+    {
+        prompt: "What is the name of this game?\n(a) Question game\n\(b) Name Game\n(c) Trivia game",
+        answer: "C"
+    },
+    {
+        prompt: "Where do babies come from?\n(a) Storks\n\(b) Birds and Bees\n(c) Amazon",
+        answer: "c"
+    },
+    {
+        prompt: "Do you like Javascript?\n(a) I LOVE JAVASCRIPT!\n\(b) I don't drink coffee\n(c) I Hate Javascript",
+        answer: "c"
+    },
+    {
+        prompt: "Do you complete your homework when you are supposed to?\n(a) My Dog ate my Homework\n\(b) Sometimes \n(c) There was homework?",
+        answer: "a"
+    },
+    {
+        prompt: "What is the meaning of life?\n(a) Love!\n\(b) 42\n(c) Definitely not Javascript",
+        answer: "b"
+    }
+]
+
+//variables to set the score at zero and a place to track wins and losses
+var wins = 0;
+var losses = 0;
+
+//here i created a "for" loop that will run through my question variable, cycling through all the questions. 
+for (var i = 0; i < question.length; i++) {
+    //now I create a variable for the users response
+    var response = window.prompt(question[i].prompt);
+    //now we need an if statement to check if the answer is correct. if so then it will add to the score variable and then alert the user correct. If it is not correct then it is automatically sent to the else statement and alert incorrect.
+    if (response == question[i].answer) {
+        wins++;
+        alert("Correct!");
+    } else {
+        losses++
+        alert("Incorrect!");
+    }
+}
+
+document.write()
